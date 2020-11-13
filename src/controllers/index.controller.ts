e83b779e1f482d07ce7c4ecd755ae2cfaa289a67
unknown <camilo.acevedo@evo-ion.com>
2020-11-13 15:59:29 -0500

    first commit

---
+++ src/controllers/index.controller.ts
@@ -0,0 +1,67 @@
+import {Request, Response} from 'express'
+import { QueryResult } from 'pg';
+
+import { pool } from "../database";
+
+export const getUsers = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const response: QueryResult = await pool.query("SELECT * FROM users");
+        return res.status(200).json(response.rows);
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json('Internal Server error');
+    }
+}
+
+export const getUserbyId = async (req:Request, res:Response): Promise<Response> => {
+    try {
+        const id = parseInt(req.params.id)
+        const response: QueryResult = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+        return res.status(200).json(response.rows);
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json('Internal Server error');
+    }
+}
+
+ export const createUser = async (req:Request, res:Response): Promise<Response> => {
+     try {
+        const { name, email } = req.body;
+        await pool.query("INSERT INTO users (name, email) VALUES ($1, $2)", [name, email]);
+        return res.json({
+            message: 'User created successfully',
+            body: {
+                name,
+                email
+            }
+        });
+     } catch (e) {
+        console.log(e)
+        return res.status(500).json('Internal Server error');
+     }
+}
+
+export const updateUser = async (req:Request, res:Response): Promise<Response> => {
+    try {
+        const id = parseInt(req.params.id);
+        const { name, email } = req.body;
+
+        await pool.query("UPDATE users SET name = $1, email = $2 WHERE id = $3", [name, email, id]);
+        return res.json(`User ${id} updated successfully`);
+
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json('Internal Server error');
+    }
+}
+
+ export const deleteUser = async (req:Request, res:Response) => {
+    try {
+        const id =  parseInt(req.params.id);
+        await pool.query("DELETE FROM users where id = $1", [id]);
+        res.json(`User ${id} deleted successfully`);
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json('Internal Server error');
+    }
+} 
---
+++ src/database.ts
@@ -0,0 +1,8 @@
+import { Pool } from 'pg';
+
+export const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    password: '4w5nz^85SX#vz3',
+    database: 'firstdb'
+});
